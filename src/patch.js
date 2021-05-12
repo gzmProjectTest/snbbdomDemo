e733af0e9dba2ca6245314e2d36933af6d478e44
guozhiming <2522692552@qq.com>
2021-05-12 13:08:34 +0800

    测试

--- src/patch.js
+++ src/patch.js
@@ -27,3 +27,12 @@ export default function(oldNode, newNode) {
  * 
  * 
  */
+/**
+ * patch 函数，
+ * 1.旧节点与新节点对比，如果旧的为dom需要把他转化为虚拟节点，
+ * 2.然后在判断新旧节点与新节点是否相等， 
+ *  2.1如果不等，暴力删除，并设置标记，等待新节点插入，（需要找到旧节点的elm元素，pivot.parentNode.insertBefore(新的元素节点，标记点)
+ *  2.2 如果相等，递归
+ * 
+ * 
+ */
